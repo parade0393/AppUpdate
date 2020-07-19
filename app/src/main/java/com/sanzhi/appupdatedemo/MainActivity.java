9d6133c043852485bd343366913b58557f575692
parade岁月 <13703937803@163.com>
2020-07-19 22:07:46 +0800

    修改demo

--- app/src/main/java/com/sanzhi/appupdatedemo/MainActivity.java
+++ app/src/main/java/com/sanzhi/appupdatedemo/MainActivity.java
@@ -6,8 +6,13 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.sanzhi.appupdate.AppUpdate;
+import com.sanzhi.appupdate.CommonDialog;
+import com.sanzhi.appupdate.MessageDialog;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -18,10 +23,34 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MessageDialog.getInstance(MainActivity.this).build()
+                        .setOnPositionBtnClickListener(new MessageDialog.OnPositionBtnClickListener() {
+                            @Override
+                            public void onPositionBtnClick(CommonDialog commonDialog, View view) {
+                                commonDialog.dismiss();
+                            }
+                        })
+                        .setOnNegativeBtnClickListener(new MessageDialog.OnNegativeBtnClickListener() {
+                            @Override
+                            public void onNegativeBtnClick(CommonDialog commonDialog, View view) {
+                                commonDialog.dismiss();
+                                Toast.makeText(MainActivity.this,"文本",Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .show();
+            }
+        });
+
 
         checkUpdate();
     }
@@ -38,6 +67,7 @@ public class MainActivity extends AppCompatActivity {
         AppUpdate.getInstance(this)
                 .setTitle("标题")
                 .setTitleSize(17)
+                .setContentColor(0xffff0000)
                 .setAppId("nfy7qt3618gpuhv2")
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .update();
